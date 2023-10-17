e733e2f24691f2475cdc293b132bbb4f23ff1b4f
Mutukukioko <kharrylungu@gmail.com>
2023-10-17 21:01:37 +0300

    added something

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import './App.css';
+import Navbar from './Navbar';
+
+
+function App() {
+  return (
+    <div className="app">
+    <Navbar />
+      <header className="header">Chama Kenya App</header>
+          
+    </div>
+  );
+}
+
+export default App;
